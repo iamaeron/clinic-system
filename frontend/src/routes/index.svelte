34aa8c0181a215c5140d0255b0064a40914b4a00
Aeron <senciljohnaeron@gmail.com>
2025-01-11 11:20:27 +0800

    mann prettier whyyy

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script>
+  import TextInput from "../ui/TextInput.svelte";
+
 	// const s = fetch('/api/signin', { method: 'POST' }).then(r => r.text()).then(d => console.log(d));
 </script>
 
@@ -11,9 +13,14 @@
 		</div>
 	</div>
 	<div class="max-w-xl w-full flex relative">
-		<div class="flex-1 bg-white flex items-center px-6">
-			<div>
+		<div class="flex-1 bg-white flex items-center px-10">
+			<div class="w-full">
 				<h1 class="text-3xl font-semibold tracking-tight text-[#0B1725]">Sign in</h1>
+				<p class="text-zinc-600 mb-4">If you have an account, you can easily log in.</p>
+				<div class="space-y-4">
+					<TextInput />
+					<TextInput />
+				</div>
 			</div>
 		</div>
 	</div>
---
+++ frontend/src/ui/InputLabel.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { createLabel, melt } from '@melt-ui/svelte';
+  
+    const {
+      elements: { root },
+    } = createLabel();
+
+    const { required, children } = $props()
+  </script>
+  
+      <label
+        use:melt={$root}
+        for="email"
+        class="block text-sm font-semibold text-zinc-600"
+        data-melt-part="root"
+      >
+      {children}
+        {#if required}
+        <span class="inline ml-1 text-rose-600">*</span>
+            {/if}
+    </label>
